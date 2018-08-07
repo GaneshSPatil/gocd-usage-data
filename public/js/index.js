5eb5fbc8a38db87cef8f2810cbb88fb02a16ebb2
GaneshSPatil <ganeshpl@thoughtworks.com>
2018-08-07 19:29:32 +0530

    Add highest pipeline count based bar chart

--- public/js/index.js
+++ public/js/index.js
@@ -44,9 +44,61 @@ const renderVersionPieChart = function (stats) {
 	});
 };
 
+const renderPipelineCountBarChart = function (stats) {
+	const instanceIdentifiers = stats.map(s => s.server_id);
+
+	Highcharts.chart('pipeline_count_bar', {
+		chart: {
+			type: 'column'
+		},
+		title: {
+			text: 'Highest Number of Pipelines'
+		},
+		subtitle: {
+			text: 'Top 25 Instances'
+		},
+		xAxis: {
+			categories: instanceIdentifiers,
+			crosshair: true,
+			labels: {
+				formatter: function () {
+					return this.value.substr(0, 7);
+				}
+			}
+		},
+		yAxis: {
+			min: 0,
+			title: {
+				text: 'Number of Pipelines'
+			}
+		},
+
+		// tooltip: {
+		// 	headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+		// 	pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+		// 		'<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+		// 	footerFormat: '</table>',
+		// 	shared: true,
+		// 	useHTML: true
+		// },
+		plotOptions: {
+			column: {
+				pointPadding: 0.2,
+				borderWidth: 0
+			}
+		},
+		series: [{
+			name: 'Pipeline count',
+			data: stats.map(s => +s.pipeline_count)
+
+		}]
+	});
+};
+
 window.onload = function () {
 	$.get("api/data", function (responseData) {
 		renderGeneralStats(responseData.general_stats);
-		renderVersionPieChart(responseData.version_based_stats)
+		renderVersionPieChart(responseData.version_based_stats);
+		renderPipelineCountBarChart(responseData.pipeline_count_based_stats);
 	});
 };
